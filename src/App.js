eaededc6c6f8482c3e361b87616197a9de34d369
Zachery Morgan <97458923+ZacheryMorgan@users.noreply.github.com>
2022-04-23 11:49:22 -0400

    create-app-skeleton (#2)
    * install fonts
    
    * add normalize.css
    
    * add GlobalStyles and variable fonts
    
    * add router with routes to / and /search
    
    * change app title and meta description

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import GlobalStyle from './GlobalStyles';
+
+import Home from './components/Home';
+import Search from './components/Search';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <>
+      <Router>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
