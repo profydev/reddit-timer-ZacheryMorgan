cbf42209263bfb66cf8f9151a070d2e76cc31554
ZacheryShawne <zachery.shawne@gmail.com>
2022-04-23 19:23:41 -0400

    add link tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,27 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders App', () => {
-  render(<App />);
+describe('Header', () => {
+  test('Header "logo" links route correctly', () => {
+    render(
+      <App />,
+    );
+    const homeLogo = screen.getByAltText(/logo/i);
+    userEvent.click(homeLogo);
+    expect(
+      screen.getByRole('heading', { name: /home/i }),
+    ).toBeInTheDocument();
+  });
+  test('Header "search" links route correctly', () => {
+    render(
+      <App />,
+    );
+    const searchLink = screen.getByRole('link', { name: /search/i });
+    userEvent.click(searchLink);
+    expect(
+      screen.getByText(/javascript/i),
+    ).toBeInTheDocument();
+  });
 });
