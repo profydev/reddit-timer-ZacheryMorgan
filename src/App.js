7f89c1c81c3f710009d4d06cb2fa4742c129b38a
ZacheryShawne <zachery.shawne@gmail.com>
2022-04-22 21:46:31 -0400

    add GlobalStyles and variable fonts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import GlobalStyle from './GlobalStyles';
+
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <>
+      <GlobalStyle />
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import App from './App';
 
 import 'normalize.css';
 
-import '@fontsource/bitter';
-import '@fontsource/montserrat';
+import '@fontsource/bitter/400.css';
+import '@fontsource/bitter/variable.css';
+import '@fontsource/montserrat/400.css';
+import '@fontsource/montserrat/variable.css';
 
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'));
