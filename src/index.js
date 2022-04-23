dbee9fb0252a5310a5774e3b9a26ef3ce13a5696
ZacheryShawne <zachery.shawne@gmail.com>
2022-04-22 21:38:55 -0400

    install fonts

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import '@fontsource/bitter';
+import '@fontsource/montserrat';
+
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'));
